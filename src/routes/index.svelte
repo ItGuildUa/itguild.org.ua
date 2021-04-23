81f8ec5e70274d6905b73be42f4f483faad0e072
Vladimir Rozhkov <xpunkerx@gmail.com>
2021-04-23 03:34:42 +0300

    Add ids to links.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,7 +34,7 @@
             </div>
             <div class="mt-6 sm:max-w-lg sm:w-full sm:flex">
               <div class="space-x-2 flex">
-                <a href="https://www.patreon.com/itguildukraine"
+                <a href="https://www.patreon.com/itguildukraine" id="pateron"
                   class="flex items-center justify-center rounded-md border border-transparent px-3 py-3 bg-red-600 text-base font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:px-4"
                   target="_blank">
                   Приєднатися
@@ -44,7 +44,7 @@
                     </svg>
                   </span>
                 </a>
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLSegBeWsOUHFEAwTgaUetCU6mylzTdHJHY1e0ESYY5EAyaaXRw/viewform"
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSegBeWsOUHFEAwTgaUetCU6mylzTdHJHY1e0ESYY5EAyaaXRw/viewform" id="google-form"
                    class="flex items-center justify-center rounded-md border border-transparent px-3 py-3 bg-gray-600 text-base font-medium text-white shadow hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:px-4"
                    target="_blank">
                   Заповнити анкету
