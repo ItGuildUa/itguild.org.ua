6a5e0e1cb4e918e30d451b4fd5206738ed690da4
Viktoria Muzychko <v.muzychko@busylamp.com>
2021-06-01 18:00:01 +0300

    Added Code of Conduct

--- src/components/pages/Home.svelte
+++ src/components/pages/Home.svelte
@@ -8,12 +8,14 @@
         "Наша мета — представляти та захищати інтереси ІТ фахівців перед державою та бізнесом.",
       joinBtn: "Приєднатися",
       tgBtn: "Телеграм",
+      cocBtn: "Code of conduct"
     },
     en: {
       title:
         "Our mission — protect rights and interests of all Ukrainian IT specialists",
       joinBtn: "Join",
       tgBtn: "Telegram",
+      cocBtn: "Code of conduct"
     },
   };
 
@@ -86,6 +88,16 @@
                         d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"
                       />
                     </svg>
+                  </span>
+                </a>
+                <a
+                        href="/coc"
+                        id="coc"
+                        class="flex items-center justify-center rounded-md border border-transparent px-3 py-3 bg-green-500 text-base font-medium text-white shadow hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 sm:px-4"
+                        target="_blank"
+                >
+                  {locale.cocBtn}
+                  <span class="ml-2">
                   </span>
                 </a>
               </div>
